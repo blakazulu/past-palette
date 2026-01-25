cef06d11c00219e9cfb6176803d347b8d94cb467
lirazshaka <liraz@mypart.com>
2026-01-25 02:07:21 +0200

    feat(gallery): add elements barrel export

---
+++ src/components/gallery-tour/elements/index.ts
@@ -0,0 +1,8 @@
+// src/components/gallery-tour/elements/index.ts
+
+export { GalleryColumns } from './GalleryColumns';
+export { CentralPedestal } from './CentralPedestal';
+export { WallAlcoves } from './WallAlcoves';
+export { FloorMosaic } from './FloorMosaic';
+export { GalleryPlants } from './GalleryPlants';
+export { GalleryStatues } from './GalleryStatues';
